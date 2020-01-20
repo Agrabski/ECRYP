dde2260af7cbeb7bdf59df6c90df41f5dca0a785
Adam Grabski <adam.gr@outlook.com>
2020-01-20 18:42:59 +0100

    c

--- MD5.py
+++ MD5.py
@@ -5,67 +5,55 @@ from math import sin
 
 def Md5(buffer):
 	bits = PaddTo512(BitVector(textstring = buffer))
-
-	A = 0x67452301
-	B = 0xefcdab89
-	C = 0x98badcfe
-	D = 0x10325476
-
-	rounds = [
-		lambda a, b, c: (a & b) | (~a & c),
-		lambda a, b, c: (a & c) | (a & ~c),
-		lambda a, b, c: a ^ b ^ c,
-		lambda a, b, c: b ^ (a & ~c)
-	]
+	print(8)
+	a0 = 0x67452301
+	b0 = 0xefcdab89
+	c0 = 0x98badcfe
+	d0 = 0x10325476
 	constants = [int(4294967296 * abs(sin(i + 1))) for i in range(64)]
-	for block in range(0, (bits.length() // 16) - 1):
-		X = 0
-		for i in range(0, 15):
-			X |= bits[block * 16 + i] << i
-		temporary = [A,B,C,D]
-		i = 1
-		shiftStarts = [7, 5, 4, 6]
-		shiftChanges = [
-			[0,5, 10, 15],
-			[0,4, 9, 15],
-			[0,7, 12, 19],
-			[0,4, 9, 15],
-		]
-		indeces = [
-			[0, 1, 2, 3],
-			[3, 0, 1, 2],
-			[2, 3, 0, 1],
-			[1, 2, 3, 0]
-		]
-		kStarts = [0, 1, 5, 0]
-		kChanges = [1, 5, 3, 7]
-		for round in range(4):
-			#print(round)
-			for y in range(4):
-				for x in range(4):
-					#print('[{:1}{:1}{:1}{:1}|{:2},{:2},{:2}]'.format(indeces[x][0], indeces[x][1], indeces[x][2], indeces[x][3],(kStarts[round] + kChanges[round]*(x + 4*y)) % 16,shiftStarts[round] + shiftChanges[round][x],i), end=" ")
-					temporary[x] = PerformRound(
-						rounds[round],
-						temporary[indeces[x][0]],
-						temporary[indeces[x][1]],
-						temporary[indeces[x][2]],
-						temporary[indeces[x][3]],
-						(kStarts[round] + kChanges[round]*(x + 4*y)) % 16,
-						shiftStarts[round] + shiftChanges[round][x],
-						i,X,constants) % (2** 32)
-					i += 1
-				#print('\n')
-		A = (A + temporary[0]) % (2 ** 32)
-		B = (B + temporary[1]) % (2 ** 32)
-		C = (C + temporary[2]) % (2 ** 32)
-		D = (D + temporary[3]) % (2 ** 32)
-	t = [A, B, C, D]
-	print('{:04x}'.format(A))
-	print('{:04x}'.format(B))
-	print('{:04x}'.format(C))
-	print('{:04x}'.format(D))
-	return "".join(map(lambda x: '{:04x}'.format(x),t))
-	
+	shifts = [ 7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21]
+	print(15)
+	for block in range(0, (bits.length() // 512)):
+		A = a0
+		B = b0
+		C = c0
+		D = d0
+		M = []
+		print(24)
+		for i in range(0, 16):
+			M.append(0)
+			for j in range(0,32):
+				M[i] = (M[i]<<1) | (bits[block*512 + i*32+j]<<(block*512 + i*32+j))
+		print(28)
+		for i in range(0 ,64):
+			F = 0
+			g = 0
+			if 0 <= i <= 15:
+				F = (B &C) | ((~B) & D)
+				g = i
+			print(35)
+			if 16 <= i <= 31:
+				F = (D & B) | ((~D) | C)
+				g = (5 * i + 1) % 16
+			
+			if 32 <= i <= 47:
+				F = B ^ C ^ D
+				g = (3 * i + 5) % 16
+			
+			if 48 <= i <= 63:
+				F = C ^ (B | (~D))
+				g = (7 * i) % 16
+			F = (F + A + constants[i] + M[g])% (2**32)
+			A = D% (2**32)
+			D = C% (2**32)
+			C = B% (2**32)
+			B = (B + shift(F, shifts[i]))% (2**32)
+		a0 = (a0 + A) % (2**32)
+		b0 = (b0 + B) % (2**32)
+		c0 = (c0 + C) % (2**32)
+		d0 = (d0 + D) % (2**32)
+	return "".join(map(lambda x: '{:04x}'.format(x),[a0,b0,c0,d0]))
+
 
 
 def PaddTo512(buffer):
@@ -77,18 +65,11 @@ def PaddTo512(buffer):
 	buffer += lengthInBits
 	return buffer
 
-def PerformRound(f, a, b, c, d, k, s, i,block, constants):
-	return b + shift((a + f(b, c, d) + block & shift(1 , k) + constants[i-1]), s)
 def shift(num, val):
-	result = num
-	for i in range(0,val):
-		oldest = num & (1<<31)
-		result = result <<1
-		if oldest != 0:
-			result |= 1
-	return result & 0b11111111111111111111111111111111
+	return (num << val) | (num >> (32-val))
 
-print(Md5("kupa"))
+k = Md5("kupa")
+print(k)
 m = hashlib.new("md5")
-m.update("kupa".encode('utf-8'))
+m.update("kupa".encode('ascii'))
 print(m.hexdigest())
